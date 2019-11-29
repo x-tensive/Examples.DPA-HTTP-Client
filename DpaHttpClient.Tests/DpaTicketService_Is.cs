ac13a2fd978f82cbb03b29e7a1cc86be86c77912
Anatoly Kochnev <Anatoly.Kochnev@x-tensive.com>
2019-11-29 15:02:10 +0500

    Added example for creating tickets for dispatcher

---
+++ DpaHttpClient.Tests/DpaTicketService_Is.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+
+namespace DpaHttpClient.Tests
+{
+	[TestFixture]
+	public class DpaTicketService_Is
+	{
+		private const string dpaHostUrl = "http://dpademo/";
+		private const string login = "operator";
+		private const string password = "123";
+		private const long expectedTicketId = 309280859;
+		private const long expectedEquipmentId = 262136686;
+		private const string ticketName = "Жду транспорта";
+		private const string equipmentName = "Sewing Machine 1";
+
+		[Test]
+		public void AbleToGetEquipmentByName()
+		{
+			using (var client = new DpaClient(dpaHostUrl))
+			{
+				client.Login(login, password);
+				var service = new DpaTicketService(client);
+				var equipmentId = service.GetEquipmentByName(equipmentName);
+
+				Assert.AreEqual(expectedEquipmentId, equipmentId);
+			}
+		}
+
+		[Test]
+		public void AbleToGetTicketByName()
+		{
+			using (var client = new DpaClient(dpaHostUrl))
+			{
+				client.Login(login, password);
+				var service = new DpaTicketService(client);
+				var ticketWasFound = service.TryGetTicketByName(ticketName, out var ticketId);
+
+				Assert.IsTrue(ticketWasFound);
+				Assert.AreEqual(expectedTicketId, ticketId);
+			}
+		}
+
+		[Test]
+		public void AbleToAddAwaitingForTransportTicket()
+		{
+			using (var client = new DpaClient(dpaHostUrl))
+			{
+				client.Login(login, password);
+
+				var service = new DpaTicketService(client);
+				service.AddAwaitingForTransportTicket(equipmentName);
+			}
+		}
+	}
+}
---
+++ DpaHttpClient/DpaTicketService.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+namespace DpaHttpClient
+{
+	public class DpaTicketService
+	{
+		private class IdNameContainer
+		{
+			public long Id { get; set; }
+			public string Name { get; set; }
+		}
+
+		private const string AWAITING_FOR_TRANSPORT_TICKET_NAME_RUS = "Жду транспорта";
+		private const string AWAITING_FOR_TRANSPORT_TICKET_NAME_ENG = "Waiting for transport";
+		private const string COMMENTARY = "From MES";
+		private readonly DpaClient client;
+
+		public DpaTicketService(DpaClient client)
+		{
+			this.client = client;
+		}
+
+		private void AddTicket(long equipmentId, long ticketId)
+		{
+			var url = "/api/ticket/createTicket/";
+			var postParams = JsonConvert.SerializeObject(new
+			{
+				EquipmentId = equipmentId,
+				TicketTypeId = ticketId,
+				Comment = COMMENTARY
+			});
+			client.Post(url, postParams);
+		}
+
+		private Exception NotFoundException(string entity, string name)
+		{
+			return new Exception($"{entity} does not found by name {name}");
+		}
+
+		public bool TryGetTicketByName(string ticketName, out long id)
+		{
+			id = -1;
+			var result = client.Get("/api/ticket/getTicketTypes/");
+			var resultTickets = JsonConvert.DeserializeObject<IdNameContainer[]>(result);
+			var resultTicket = resultTickets
+				.Where(x => x.Name == ticketName)
+				.SingleOrDefault();
+			if (resultTicket == null)
+				return false;
+			id = resultTicket.Id;
+			return true;
+		}
+
+		public long GetEquipmentByName(string equipmentName)
+		{
+			var filter = new GridRequestOptions()
+			{
+				Filter = new[] {
+					"Name",
+					"contains",
+					equipmentName
+				}
+			};
+			var url = "/api/dpaEnterpriseStrusture/getEquipments/";
+
+			var postParams = JsonConvert.SerializeObject(filter);
+			var result = client.Post(url, postParams);
+
+			if (string.IsNullOrEmpty(result))
+				throw NotFoundException("Equipment", equipmentName);
+
+			var resultObject = JsonConvert.DeserializeObject<IdNameContainer[]>(result);
+			if (resultObject.Length == 0)
+				throw NotFoundException("Equipment", equipmentName);
+
+			return resultObject[0].Id;
+		}
+
+		public void AddAwaitingForTransportTicket(string equipmentName)
+		{
+			var equipmentId = GetEquipmentByName(equipmentName);
+			if (!TryGetTicketByName(AWAITING_FOR_TRANSPORT_TICKET_NAME_RUS, out long ticketId))
+			{
+				if (!TryGetTicketByName(AWAITING_FOR_TRANSPORT_TICKET_NAME_ENG, out ticketId))
+				{
+					throw NotFoundException("Ticket", AWAITING_FOR_TRANSPORT_TICKET_NAME_RUS);
+				}
+			}
+
+			AddTicket(equipmentId, ticketId);
+		}
+	}
+}
